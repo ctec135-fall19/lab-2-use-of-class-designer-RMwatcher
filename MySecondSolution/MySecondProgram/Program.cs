74fac73c699b6a6043d92281f1c6baac094a380f
Richard <r.mora@students.clark.edu>
2019-10-03 14:10:03 -0700

    Lab 2 compelete (better late than never)

---
+++ MySecondSolution/MySecondProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySecondProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("This is how you can input strings onto the console");
+
+            SportsCar myCar = new SportsCar();
+
+            Console.WriteLine("My Car: {0}", myCar.GetPetName());
+        }  
+        
+        }
+            
+
+        }
+            
+        
+    
+
